9c4c2e7dacd3a6ceec375104a1f927e02107f66c
Skivman <al.sitkevich@gmail.com>
2021-10-11 18:45:07 +0300

    Переименование random-objects,перенос констант в модуль моковых объектов

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-import {getNewUser, getNewLocation, getNewOffer} from './random-objects';
-
-const getOffer = () => ({
-  author: getNewUser(),
-  location: getNewLocation(),
-  offer: getNewOffer(),
-});
-
-const getMockAds = () => new Array(10).fill('').map(getOffer);
+import {getMockAds} from './mock-data';
 
 getMockAds();
